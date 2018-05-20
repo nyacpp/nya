c716f9f80f31832e94db6771d616a9e1152e1ac8
Akela1101 <akela1101@gmail.com>
2018-05-21 00:00:28 +0900

    fix easylogging build

--- cmake/nya.cmake
+++ cmake/nya.cmake
@@ -11,7 +11,7 @@ add_definitions(-DBOOST_MOVE_USE_STANDARD_LIBRARY_MOVE) # std::move and boost::m
 add_definitions(-DWIN32_LEAN_AND_MEAN)                  # for boost::asio - reduces windows.h
 
 # easylogging
-set(build_static_lib ON cache)
+set(build_static_lib ON CACHE BOOL "Build easyloggingpp as a static library" FORCE)
 add_definitions(-DELPP_NO_DEFAULT_LOG_FILE)             # custom log file
 add_definitions(-DELPP_WINSOCK2)                        # for boost::asio
 add_definitions(-DELPP_STL_LOGGING)                     # stl
