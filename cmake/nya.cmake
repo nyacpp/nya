154cec90a5ec6e434af511d5fedbbef27ea013df
Akela1101 <akela1101@gmail.com>
2019-02-16 18:18:41 +0900

    cmake scripts update

--- cmake/nya.cmake
+++ cmake/nya.cmake
@@ -1,6 +1,3 @@
-set(CMAKE_CXX_STANDARD 17)
-set(CMAKE_CXX_STANDARD_REQUIRED on)
-
 # definitions
 add_definitions(-DBOOST_MOVE_USE_STANDARD_LIBRARY_MOVE) # std::move and boost::move won't interfere
 add_definitions(-DWIN32_LEAN_AND_MEAN)                  # for boost::asio - reduces windows.h
